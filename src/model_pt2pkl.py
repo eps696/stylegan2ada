61ac8a4632efdbc6380f4d7b79f5c88acccb7cf5
Vadim Epstein <eps@here.ru>
2021-12-28 15:31:55 +0300

    cleanup

--- src/model_pt2pkl.py
+++ src/model_pt2pkl.py
@@ -18,7 +18,6 @@ def get_args():
     parser = argparse.ArgumentParser(description="Rosinality (pytorch) to Nvidia (pkl) checkpoint converter")
     parser.add_argument("--model_pkl", metavar="PATH", help="path to the source sg2ada pytorch (pkl) weights")
     parser.add_argument("--model_pt", metavar="PATH", help="path to the updated pytorch (pt) weights")
-    parser.add_argument("--channel_multiplier", type=int, default=2, help="channel multiplier factor. config-f = 2, else = 1")
     args = parser.parse_args()
     return args
 
@@ -51,23 +50,6 @@ def convert_torgb(tgt_dict, src_dict, target_name, source_name):
 def convert_dense(tgt_dict, src_dict, target_name, source_name):
     update(tgt_dict, target_name + ".weight", src_dict[source_name + ".weight"])
     update(tgt_dict, target_name + ".bias",   src_dict[source_name + ".bias"])
-
-def update_D(src_dict, tgt_dict, size, n_mlp):
-    log_size = int(math.log(size, 2))
-    convert_conv(tgt_dict, src_dict, f"{size}x{size}/FromRGB", "convs.0")
-    conv_i = 1
-    pbar = ProgressBar(log_size-1)
-    for i in range(log_size-2, 0, -1):
-        reso = 4 * 2 ** i
-        convert_conv(tgt_dict, src_dict, f"{reso}x{reso}/Conv0",      f"convs.{conv_i}.conv1")
-        convert_conv(tgt_dict, src_dict, f"{reso}x{reso}/Conv1_down", f"convs.{conv_i}.conv2", start=1)
-        convert_conv(tgt_dict, src_dict, f"{reso}x{reso}/Skip",       f"convs.{conv_i}.skip",  start=1, bias=False)
-        conv_i += 1
-        pbar.upd()
-    convert_conv(tgt_dict, src_dict,  f"4x4/Conv",   "final_conv")
-    convert_dense(tgt_dict, src_dict, f"4x4/Dense0", "final_linear.0")
-    convert_dense(tgt_dict, src_dict, f"Output",     "final_linear.1")
-    pbar.upd()
 
 def update_G(src_dict, tgt_dict, size, n_mlp):
     log_size = int(math.log(size, 2))
