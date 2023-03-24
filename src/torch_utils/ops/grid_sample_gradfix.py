e500a0fa387ed5ce56b8f767f077bf91270884c8
vadim epstein <eps@here.ru>
2023-03-24 17:03:52 +0300

    fix for new torch

--- src/torch_utils/ops/grid_sample_gradfix.py
+++ src/torch_utils/ops/grid_sample_gradfix.py
@@ -58,6 +58,8 @@ class _GridSample2dBackward(torch.autograd.Function):
     @staticmethod
     def forward(ctx, grad_output, input, grid):
         op = torch._C._jit_get_operation('aten::grid_sampler_2d_backward')
+# !!! fix for pytorch > 1.10
+        if isinstance(op, tuple) or len(op)==2: op = op[0]
         if _use_pytorch_1_11_api:
             output_mask = (ctx.needs_input_grad[1], ctx.needs_input_grad[2])
             grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False, output_mask)
