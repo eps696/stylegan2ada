35b266dcf460609ae1b7b10cce95f29f8f605806
vadim epstein <eps@here.ru>
2024-04-18 22:58:11 +0300

    bat update

--- gen.bat
+++ gen.bat
@@ -8,17 +8,28 @@ if "%1"=="" goto help
 if "%2"=="" goto test
 if "%2"=="1" goto test
 
-python src/_genSGAN2.py --model models/%1 --out_dir _out/%~n1 --size %2 --frames %3 ^
-%4 %5 %6 %7 %8 %9
+set model=%1
+set name=%~n1
+set size=%2
+set frames=%3
+set args=%4 %5 %6 %7 %8 %9
+for %%q in (1 2 3 4 5 6 7 8 9 10) do shift 
+set args=%args% %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+for %%q in (1 2 3 4 5 6 7 8 9 10) do shift 
+set args=%args% %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+for %%q in (1 2 3 4 5 6 7 8 9 10) do shift 
+set args=%args% %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+python src/_genSGAN2.py --model models/%model% --out_dir _out/%name% --size %size% --frames %frames%  %args%
 goto ff
 
 :test
-python src/_genSGAN2.py --model models/%~n1.pkl --out_dir _out/%~n1 --frames 200-20 ^
+python src/_genSGAN2.py --model models/%name%.pkl --out_dir _out/%name% --frames 200-20 ^
 %3 %4 %5 %6 %7 %8 %9
 
 :ff
-ffmpeg -y -v warning -i _out\%~n1\%%06d.jpg -c:v mjpeg -q:v 2 _out/%~n1-%2.avi
-rem rmdir /s /q _out\%~n1
+ffmpeg -y -v warning -i _out\%name%\%%06d.jpg -c:v mjpeg -q:v 2 _out/%name%-%2.avi
+rem rmdir /s /q _out\%name%
 
 goto end
 
